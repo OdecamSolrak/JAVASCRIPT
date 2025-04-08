a4fa547dfec46c8e16876bd9484ea76d7522aa47
OdecamSolrak <gomide.nando@gmail.com>
2025-04-08 18:47:22 -0300

    novo

---
+++ condicoes/001.js
@@ -0,0 +1,8 @@
+var vel = 70
+console.log(`A velocidade do carro é ${vel} Km/h `)
+if (vel > 60) { 
+    console.log('Você foi multado por ultrapassar o limite de velocidade permitido!')
+}
+
+console.log('Dirija sempre utilizando o cinto de segurança.')
+
---
+++ condicoes/002.js
@@ -0,0 +1,8 @@
+var país = 'Brasil'
+
+console.log(`Morando no ${país}`)
+if (país == 'Brasil') {
+    console.log('Você é Brasileiro')
+} else {
+    console.log('Você é estrangeiro')
+}
